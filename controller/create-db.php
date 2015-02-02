033568c9c412a61892e67f26d982585629bcdb95
DakotaD <dakota.degracia@gmail.com>
2015-02-02 15:05:03 -0800

    make it able to post

--- controller/create-db.php
+++ controller/create-db.php
@@ -10,16 +10,28 @@ if ($connection->connect_error) {
 
 $exists = $connection->select_db($database);
 
-if (!$exists) {
-    $query = $connection->query("CREATE DATABASE  $database");
+if(!$exists) {
+    $query = $connection->query("CREATE DATABASE $database");
 
-    if ($query) {
-       echo"Successfully created  database" . $database;
+    if($query) {
+       echo"Successfully created  database: " . $database;
     }
 } 
 else {
-   echo"Database has already exists.";
+   echo "Database already exists.";
 }
 
+$query = $connection->query ("CREATE TABLE posts("
+        . "id int (11) NOT NULL AUTO_INCREMENT,"
+        . "title varchar(255) NOT NULL,"
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+if($query){
+    echo "Succesfully create table : posts";
+}
+else{
+    echo "$connection->error";
+}
 $connection->close();
 
